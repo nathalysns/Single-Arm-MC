fc2224a7eaa99d2712d6c369ade02a814816461c
nathaly <ssanties@jlab.org>
2019-03-18 13:50:46 -0400

    deleted:    phase_space_fall16_LHRS/Makefile 	deleted:    phase_space_fall16_LHRS/constants.inc 	deleted:    phase_space_fall16_LHRS/enerloss_new.f 	deleted:    phase_space_fall16_LHRS/g_dump_all_events.inc 	deleted:    phase_space_fall16_LHRS/gauss1.f 	deleted:    phase_space_fall16_LHRS/hrsl/apertures_hrsl.inc 	deleted:    phase_space_fall16_LHRS/hrsl/hrs-sos-barak.fox 	deleted:    phase_space_fall16_LHRS/hrsl/hrs-sos-rays.fox 	deleted:    phase_space_fall16_LHRS/hrsl/hrs_forward_cosy.dat 	deleted:    phase_space_fall16_LHRS/hrsl/hrs_recon_cosy.dat 	deleted:    phase_space_fall16_LHRS/hrsl/mc_hrsl.d 	deleted:    phase_space_fall16_LHRS/hrsl/mc_hrsl.f 	deleted:    phase_space_fall16_LHRS/hrsl/mc_hrsl_hut.d 	deleted:    phase_space_fall16_LHRS/hrsl/mc_hrsl_hut.f 	deleted:    phase_space_fall16_LHRS/hrsl/mc_hrsl_recon.d 	deleted:    phase_space_fall16_LHRS/hrsl/mc_hrsl_recon.f 	deleted:    phase_space_fall16_LHRS/hrsl/mc_hrsl_savedfc.f 	deleted:    phase_space_fall16_LHRS/hrsl/struct_hrsl.inc 	deleted:    phase_space_fall16_LHRS/infiles/ar_c.inp 	deleted:    phase_space_fall16_LHRS/infiles/carbon_foil.inp 	deleted:    phase_space_fall16_LHRS/infiles/d2.inp 	deleted:    phase_space_fall16_LHRS/infiles/dummy.inp 	deleted:    phase_space_fall16_LHRS/infiles/generated_events.dat 	deleted:    phase_space_fall16_LHRS/infiles/h3.inp 	deleted:    phase_space_fall16_LHRS/infiles/he3.inp 	deleted:    phase_space_fall16_LHRS/infiles/rand.dat 	deleted:    phase_space_fall16_LHRS/infiles/runplan.inp 	deleted:    phase_space_fall16_LHRS/infiles/up.inp 	deleted:    phase_space_fall16_LHRS/loren.f 	deleted:    phase_space_fall16_LHRS/mc_hrs_single.f 	deleted:    phase_space_fall16_LHRS/mt19937.f 	deleted:    phase_space_fall16_LHRS/run.sh 	deleted:    phase_space_fall16_LHRS/shared/CVS/Entries 	deleted:    phase_space_fall16_LHRS/shared/CVS/Repository 	deleted:    phase_space_fall16_LHRS/shared/CVS/Root 	deleted:    phase_space_fall16_LHRS/shared/locforunt.f 	deleted:    phase_space_fall16_LHRS/shared/musc.f 	deleted:    phase_space_fall16_LHRS/shared/musc_ext.f 	deleted:    phase_space_fall16_LHRS/shared/project.f 	deleted:    phase_space_fall16_LHRS/shared/rotate_haxis.f 	deleted:    phase_space_fall16_LHRS/shared/stringlib.f 	deleted:    phase_space_fall16_LHRS/shared/transp.f 	deleted:    phase_space_fall16_LHRS/simulate.inc 	deleted:    phase_space_fall16_LHRS/spectrometers.inc 	deleted:    phase_space_fall16_LHRS/temp.log 	deleted:    phase_space_fall16_LHRS/testgaus.C

--- phase_space_fall16_LHRS/hrsl/mc_hrsl_hut.d
+++
@@ -1,3 +0,0 @@
-hrsl/mc_hrsl_hut.o: hrsl/struct_hrsl.inc
-hrsl/mc_hrsl_hut.o: hrsl/../spectrometers.inc
-hrsl/mc_hrsl_hut.o: hrsl/../g_dump_all_events.inc
--- phase_space_fall16_LHRS/hrsl/mc_hrsl_recon.d
+++
@@ -1,1 +0,0 @@
-hrsl/mc_hrsl_recon.o: hrsl/../spectrometers.inc
